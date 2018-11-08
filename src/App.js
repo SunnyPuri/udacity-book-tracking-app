2852bb578143c74d5b32211adf5e09b1a192fb59
Sunny Puri <sunnypuri2002@gmail.com>
2018-11-08 23:08:17 +0530

    Book Component Added

--- src/App.js
+++ src/App.js
@@ -1,19 +1,96 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+
 import './App.css';
+import './assets/css/loader.css';
+
+import * as BooksAPI from './components/book/BooksAPI';
+import BookShelf from './components/book/BookShelf';
+import BookSearch from './components/book/BookSearch';
 
 class App extends Component {
+
+  state = {
+		books: [],
+		isLoaded: false
+	};
+
+	componentDidMount() {
+		BooksAPI.getAll().then(res => {
+			this.setState({
+				books: res,
+				isLoaded: true
+			});
+		});
+  }
+  
+  changeShelf = (book, event) => {
+		return new Promise(resolve => {
+			BooksAPI.update(book, event.target.value).then(res => {
+				BooksAPI.getAll().then(res => {
+					this.setState(
+						{
+							books: res
+						},
+						resolve(res)
+					);
+				});
+			});
+		});
+	};
+
+
   render() {
+
+    const { isLoaded, books } = this.state;
     return (
       <div className="app">
+        
         <Route
 					exact
 					path="/"
 					render={() => (
-              <div>
-                Home
+            <div className="list-books">
+              <div className="list-books-title">
+                <h1>MyReads</h1>
               </div>
+              <div className="list-books-content">
+                <div>
+                  <BookShelf
+                    key="Currently Reading"
+                    shelfName="Currently Reading"
+                    appLoaded={isLoaded}
+                    fBooks={books.filter(book => book.shelf === 'currentlyReading')}
+                    onChangeShelf={this.changeShelf}
+                  />
+                  <BookShelf
+                    key="Want to Read"
+                    shelfName="Want to Read"
+                    appLoaded={isLoaded}
+                    fBooks={books.filter(book => book.shelf === 'wantToRead')}
+                    onChangeShelf={this.changeShelf}
+                  />
+                  <BookShelf
+                    key="Read"
+                    shelfName="Read"
+                    appLoaded={isLoaded}
+                    fBooks={books.filter(book => book.shelf === 'read')}
+                    onChangeShelf={this.changeShelf}
+                  />
+                </div>
+              </div>
+              <div className="open-search">
+                <Link to="/search">Add Book</Link>
+              </div>
+            </div>
           )}/>
+
+          <Route
+            path="/search"
+            render={() => <BookSearch shelvedBooks={books} onChangeShelf={this.changeShelf} />}
+				  />
+
       </div>
     );
   }
