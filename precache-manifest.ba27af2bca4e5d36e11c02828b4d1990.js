1848fa541f7077e50864c430edb6c78ca0fcafbb
SunnyPuri <sunnypuri2002@gmail.com>
2018-11-08 23:51:34 +0530

    Updates

---
+++ precache-manifest.ba27af2bca4e5d36e11c02828b4d1990.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "9d4e3f366f039249695de85a30a02b95",
+    "url": "/udacity-book-tracking-app/static/media/arrow-drop-down.9d4e3f36.svg"
+  },
+  {
+    "revision": "20e8847d1eb7759c19b0e30beb8849cf",
+    "url": "/udacity-book-tracking-app/static/media/arrow-back.20e8847d.svg"
+  },
+  {
+    "revision": "75d390e5eabc3254418e950df631389b",
+    "url": "/udacity-book-tracking-app/static/media/add.75d390e5.svg"
+  },
+  {
+    "revision": "44766cc23186c0a5f4ea",
+    "url": "/udacity-book-tracking-app/static/js/runtime~main.44766cc2.js"
+  },
+  {
+    "revision": "aab4edb9ff58961b5363",
+    "url": "/udacity-book-tracking-app/static/js/main.aab4edb9.chunk.js"
+  },
+  {
+    "revision": "0466faab8d245d75e33e",
+    "url": "/udacity-book-tracking-app/static/js/1.0466faab.chunk.js"
+  },
+  {
+    "revision": "aab4edb9ff58961b5363",
+    "url": "/udacity-book-tracking-app/static/css/main.bb250ba8.chunk.css"
+  },
+  {
+    "revision": "4c4dd0289e5f7ce6adf9d32d7e1099a9",
+    "url": "/udacity-book-tracking-app/index.html"
+  }
+];
